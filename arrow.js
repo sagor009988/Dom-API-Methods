7f966da5ce515e9840330ec6ceb8b81e8b269357
sagor009988 <soheltashjid001@gmail.com>
2024-02-20 10:08:24 +0600

    Es-6 Methods

--- arrow.js
+++ arrow.js
@@ -1,77 +1,78 @@
-const student={name:'sohel',age:30,country:'bangladesh'};
-const getCountry=person=>person.country;
-const cnt=getCountry(student);
-console.log(cnt);
+// const value=[{name:'sohel',age:29,nationality:'bangladesh'}];
+// console.log(value[0]);
 
-const arry=[2,4,5,77,90,44];
-const getThird=value=>value[2];
-const value=getThird(arry);
-console.log(value);
+// double the array value;
 
-const doMath=(x,y,z)=>{
-    const sum=x+y+z;
-    const Multiply=x*y*z;
-    const total=sum+Multiply;
-    return total
-}
-console.log(doMath(4,5,7));
+const numbers=[2,4,6,8,9,11,22];
+// const dobuled=[];
 
-const max=Math.max(2,44,55,3,7,9,99,10)
-console.log(max);
+// for(const num of numbers){
+//     const double=num * 2;
+//     dobuled.push(double)
+// }
+// console.log(dobuled);
 
-const array=[11,33,55,80,67,103,500];
-const arrayMaxNumber=Math.max(...array)
-console.log(arrayMaxNumber);
+// way 2
 
-
-const friends=['sohel','masud','belal'];
-
-const newFriends=[...friends,'rasel'];
-// or 
-const newNewFriends=['akash','batash',...friends,'chikku'];
-console.log(newNewFriends);
-
-// distructuring
-
-const studentName={
-    name:'sohel',
-    age:28,
-    mobile:'0182374940',
-    study:'student'
+function double(num){
+    return num * 2
 }
 
-const {name,mobile}=studentName;
-console.log(name,mobile);
+const result=numbers.map(double)
 
-function double (a,b){
-    return [a*2 , b*2]
+//  final way map
+
+const array=[2,3,4,5,6,7,8];
+
+
+const divide=array.map(nmu=>nmu /2);
+console.log(divide);
+// 
+
+
+const value=[2,4,6,7,9];
+
+const plus=value.map(val=>val + 5);
+
+console.log(plus);
+// 
+// 
+
+const friends=['mustofa','mehmed','bristy','sohel'];
+
+const letter=friends.map(letters=>letters[0]);
+console.log(letter);
+
+// for Each use
+
+const values=[2,3,5,7,8,9];
+const forEachResult=values.forEach(num=>num+11);
+console.log(forEachResult);
+
+//  filter use , filter means condition and need to full fill to this;
+
+const filterNumbers=[22,44,55,77,88];
+const filter=filterNumbers.filter(fil=>fil < 44);
+console.log(filter);
+
+const findNumbers=[22,44,55,77,88];
+const find=filterNumbers.find(fil=>fil < 44);
+console.log(find);
+
+// class constructor
+
+// class className{
+//     constructor (){...}
+// }
+
+class mobile{
+ constructor (name,price,model){
+    this.n=name;
+    this.p=price;
+    this.op=model
+ }
+    
 }
 
-const [res,p]=double(3,5)
-console.log(res,p);
-
-
-// up comment here
-
-const laptop={
-    name:'HP',
-    color:'silver',
-    price:200000,
-    ram:'8gb'
-}
-// find the keys or property of this object
-const keys=Object.keys(laptop);
-// console.log(keys);
-
-// find the values of the objects;
-
-const values=Object.values(laptop);
-// console.log(values);
-
-// find the pair/entries of object  or display the  object of object
-
-const pair=Object.freeze(laptop);
-
-delete laptop.ram;
-console.log(laptop);
-
+const mobile1=new mobile('samsung',24000,'samsung-A50');
+console.log(mobile1);
